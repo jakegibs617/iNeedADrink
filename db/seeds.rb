166afa97ae7f2418eb5a2f1998149c847d8845d9
Jacob <jakegibs617@gmail.com>
2016-01-18 00:53:02 -0500

    seed file update

--- db/seeds.rb
+++ db/seeds.rb
@@ -13,3 +13,86 @@ Drink.create(
 	season: "Spring",
 	image_url: "http://pixelatedcrumb.com/wp-content/uploads/2012/05/mint_julep-1.jpg"
 	)
+
+
+	collection = [
+	    {
+	      title: "Old Fashioned"
+	    },
+	    {
+	      title: "Martinez"
+	    },
+	    {
+	      title: "Martini"
+	    },
+	    {
+	      title: "Manhattan"
+	    },
+	    {
+	      title: "Brooklyn"
+	    },
+	    {
+	      title: "Daiquiri"
+	    },
+	    {
+	      title: "Margarita"
+	    },
+	    {
+	      title: "Sidecar"
+	    },
+	    {
+	      title: "French 75"
+	    },
+	    {
+	      title: "Bloody Mary"
+	    },
+	    {
+	      title: "Irish Coffee"
+	    },
+	    {
+	      title: "Jack Rose"
+	    },
+	    {
+	      title: "Negroni"
+	    },
+	    {
+	      title: "Boulevardier"
+	    },
+	    {
+	      title: "Sazerac"
+	    },
+	    {
+	      title: "Vieux Carré"
+	    },
+	    {
+	      title: "Ramos Gin Fizz"
+	    },
+	    {
+	      title: "Mint Julep"
+	    },
+	    {
+	      title: "Whiskey Sour"
+	    },
+	    {
+	      title: "Mai Tai"
+	    },
+	    {
+	      title: "Planter's Punch"
+	    },
+	    {
+	      title: "Cosmopolitan"
+	    },
+	    {
+	      title: "Pisco Sour"
+	    },
+	    {
+	      title: "Tom Collins"
+	    },
+	    {
+	      title: "Last Word"
+	    }
+	  ]
+
+		collection.each do |x|
+		  Drink.create( x )
+		end
