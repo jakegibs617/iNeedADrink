c86834ccaa9ddbb9ce8ef612511fc68e11fede90
Jacob <jakegibs617@gmail.com>
2016-01-18 12:39:07 -0500

    minimal achieved

--- db/seeds.rb
+++ db/seeds.rb
@@ -17,164 +17,43 @@
 	    },
 	    {
 	      title: "The L Train",
-				instructions: "Step One: shots",
-				description: "More delicious",
+				instructions: "i. Add the gin, St-Germain, lemon juice, and lavender to the shaker \n\r ii. Add ice to above the level of the liquid and shake vigorously for 10 seconds \n\r iii. Strain the mixture into chilled coupes and top with selzer. Garnish with remaining lavender sprigs.",
+				description: "Late winter in New York City is often bitterly cold and dark- and it sometimes leaves us yearning for the summer long past. To combat these bleak spells, we turn to dried lavender that we've saved from the summer's farmers markets as a reminder that warmer weather will return.",
+				ingredients: "makes two drinks \n\r 2 shots gin \n\r 1 shot St-Germain \n\r 1/2 shot fresh lemon juice \n\r 2 sprigs of lavender (plus to to garnish) \n\r Seltzer",
 				season: "Winter",
-				image_url: "http://pixelatedcrumb.com/wp-content/uploads/2012/05/mint_julep-1.jpg"
+				image_url: "http://static1.squarespace.com/static/54ac8bfee4b08dbf5e2385ea/t/54d42c1ae4b00d4a5941a2d1/1423198541669/Choo-choo"
 	    },
 	    {
 	      title: "The \'Nog",
-				instructions: "Step One: shots",
-				description: "More delicious",
+				instructions: "i. Add the bourbon, rum, ice cream, cinnamon, and nutmeg to the shaker \n\r ii. Shake vigorously for 10 seconds. \n\r iii. Strain the mixture into chilled coupes and garnish with remaining nutmeg",
+				description: "Eggnog can be a pain to make, and most store-bought versions fall flat. But we can't go through a holiday season without some of this quitessential festive drink, so here's our version of a super quick and tasty \'nog-perfect for your next holiday gathering.",
+				ingredients: "makes two drinks \n\r 1 shot bourbon \n\r 1 shot dark rum \n\r 2 scoops of vanilla ice cream \n\r 1/2 teaspoon of ground cinnamon \n\r 1/2 teaspoon of ground nutmeg (plus a pinch to garnish)",
 				season: "Winter",
-				image_url: "http://pixelatedcrumb.com/wp-content/uploads/2012/05/mint_julep-1.jpg"
-	    },
-	    {
-	      title: "Winter in the Workshop",
-				instructions: "Step One: shots",
-				description: "More delicious",
-				season: "Winter",
-				image_url: "http://pixelatedcrumb.com/wp-content/uploads/2012/05/mint_julep-1.jpg"
+				image_url: "http://1.bp.blogspot.com/-x32KoxxMf40/UNP2LVa8kaI/AAAAAAAAIX4/Y7TRToTLf4g/s1600/IMG_0294fc.jpg"
 	    },
 	    {
 	      title: "The Rye Old Fashioned",
-				instructions: "Step One: shots",
-				description: "More delicious",
+				instructions: "i. Add the cane sugar cubes and aromatic bitters to the shaker \n\r ii. Muddle the ingredients in the bottom of the shaker until the sugar has mostly disolved \n\r iii. Add the rye and orange zest to the shaker \n\r iv. Add the isce to above the level of the liquid and stir for 10 seconds. \n\r v. Strain the mixture into rocks glasses containing large cubes of ice and garnish with remaining orange zest",
+				description: "-",
+				ingredients: "makes two drinks \n\r 4 shots rye \n\r 2 cubes cane sugar \n\r 10 dashes of aromatic bitters \n\r 2 strips of orange zest (plus 2 to garnish)",
 				season: "Winter",
-				image_url: "http://pixelatedcrumb.com/wp-content/uploads/2012/05/mint_julep-1.jpg"
+				image_url: "http://media2.fdncms.com/sfweekly/imager/the-rock-and-rye-old-fashioned-with-the-rock/u/big/2628805/rock_rye_byloubustamante.jpg"
 	    },
 	    {
 	      title: "A Mid-Winter Marg",
-				instructions: "Step One: shots",
-				description: "More delicious",
+				instructions: "i. Combine the tequila, Cointreau, lime juice, and grapefruit juice in the shaker. \n\r ii. Add ice to above the level of liquid and shake vigorously for 15 seconds. \n\r iii. Strain the mixture into chilled rocks glasses rimmed with chile salt and containing large cubes of ice. Garnish with lime slices",
+				description: "Margaritas aren\'t just for the summer time. Winter brings beautiful citrus from the southern states, which we use to mix up this grapefruit-fueled marg.",
+				ingredients: "makes two drinks \n\r 3 shots tequila \n\r 1 & 1/2 shots Cointreau \n\r 1 & 1/2 shots fresh lime juice \n\r 1 & 1/2 shots fresh grapefruit juice \n\r 2 slices of fresh lime (to garnish) \n\r Chile salt",
 				season: "Winter",
-				image_url: "http://pixelatedcrumb.com/wp-content/uploads/2012/05/mint_julep-1.jpg"
+				image_url: "https://scontent.cdninstagram.com/hphotos-xap1/t51.2885-15/s320x320/e15/10899046_783007361793697_1937767798_n.jpg"
 	    },
 	    {
 	      title: "Juicin'",
-				instructions: "Step One: shots",
-				description: "More delicious",
+				instructions: "i. Add the kale, lemon juice, cane sugar cube, and ginger to the shaker. \n\r ii. Muddle the ingredients in the bottom of the shaker until thoroughly crushed and the sugar has mostly dissolved. \n\r iii. Add the apple cider and ice to above the level of the liquid and shake vigorously for 15 seconds \n\r iv. Strain the mixture into Collins glasses containing large cubes of ice.",
+				description: "Our New Year's resolutions usually include a vow to eat and drink more healthfully ... which is why we find ourselves whipping up this easy version of a green juice as winter comes to a close.",
+				ingredients: "makes two drinks \n\r 8 shots apple cider \n\r 1 large handful of kale \n\r 2 shots fresh lemon juice \n\r 1 cube cane sugar \n\r 6 slices of fresh ginger",
 				season: "Winter",
-				image_url: "http://pixelatedcrumb.com/wp-content/uploads/2012/05/mint_julep-1.jpg"
-	    },
-	    {
-	      title: "French 75",
-				instructions: "Step One: shots",
-				description: "More delicious",
-				season: "Spring",
-				image_url: "http://pixelatedcrumb.com/wp-content/uploads/2012/05/mint_julep-1.jpg"
-	    },
-	    {
-	      title: "Bloody Mary",
-				instructions: "Step One: shots",
-				description: "More delicious",
-				season: "Spring",
-				image_url: "http://pixelatedcrumb.com/wp-content/uploads/2012/05/mint_julep-1.jpg"
-	    },
-	    {
-	      title: "Irish Coffee",
-				instructions: "Step One: shots",
-				description: "More delicious",
-				season: "Spring",
-				image_url: "http://pixelatedcrumb.com/wp-content/uploads/2012/05/mint_julep-1.jpg"
-	    },
-	    {
-	      title: "Jack Rose",
-				instructions: "Step One: shots",
-				description: "More delicious",
-				season: "Spring",
-				image_url: "http://pixelatedcrumb.com/wp-content/uploads/2012/05/mint_julep-1.jpg"
-	    },
-	    {
-	      title: "Negroni",
-				instructions: "Step One: shots",
-				description: "More delicious",
-				season: "Spring",
-				image_url: "http://pixelatedcrumb.com/wp-content/uploads/2012/05/mint_julep-1.jpg"
-	    },
-	    {
-	      title: "Boulevardier",
-				instructions: "Step One: shots",
-				description: "More delicious",
-				season: "Spring",
-				image_url: "http://pixelatedcrumb.com/wp-content/uploads/2012/05/mint_julep-1.jpg"
-	    },
-	    {
-	      title: "Sazerac",
-				instructions: "Step One: shots",
-				description: "More delicious",
-				season: "Spring",
-				image_url: "http://pixelatedcrumb.com/wp-content/uploads/2012/05/mint_julep-1.jpg"
-	    },
-	    {
-	      title: "Vieux Carré",
-				instructions: "Step One: shots",
-				description: "More delicious",
-				season: "Spring",
-				image_url: "http://pixelatedcrumb.com/wp-content/uploads/2012/05/mint_julep-1.jpg"
-	    },
-	    {
-	      title: "Ramos Gin Fizz",
-				instructions: "Step One: shots",
-				description: "More delicious",
-				season: "Spring",
-				image_url: "http://pixelatedcrumb.com/wp-content/uploads/2012/05/mint_julep-1.jpg"
-	    },
-	    {
-	      title: "Mint Julep",
-				instructions: "Step One: shots",
-				description: "More delicious",
-				season: "Spring",
-				image_url: "http://pixelatedcrumb.com/wp-content/uploads/2012/05/mint_julep-1.jpg"
-	    },
-	    {
-	      title: "Whiskey Sour",
-				instructions: "Step One: shots",
-				description: "More delicious",
-				season: "Spring",
-				image_url: "http://pixelatedcrumb.com/wp-content/uploads/2012/05/mint_julep-1.jpg"
-	    },
-	    {
-	      title: "Mai Tai",
-				instructions: "Step One: shots",
-				description: "More delicious",
-				season: "Spring",
-				image_url: "http://pixelatedcrumb.com/wp-content/uploads/2012/05/mint_julep-1.jpg"
-	    },
-	    {
-	      title: "Planter's Punch",
-				instructions: "Step One: shots",
-				description: "More delicious",
-				season: "Spring",
-				image_url: "http://pixelatedcrumb.com/wp-content/uploads/2012/05/mint_julep-1.jpg"
-	    },
-	    {
-	      title: "Cosmopolitan",
-				instructions: "Step One: shots",
-				description: "More delicious",
-				season: "Spring",
-				image_url: "http://pixelatedcrumb.com/wp-content/uploads/2012/05/mint_julep-1.jpg"
-	    },
-	    {
-	      title: "Pisco Sour",
-				instructions: "Step One: shots",
-				description: "More delicious",
-				season: "Spring",
-				image_url: "http://pixelatedcrumb.com/wp-content/uploads/2012/05/mint_julep-1.jpg"
-	    },
-	    {
-	      title: "Tom Collins",
-				instructions: "Step One: shots",
-				description: "More delicious",
-				season: "Spring",
-				image_url: "http://pixelatedcrumb.com/wp-content/uploads/2012/05/mint_julep-1.jpg"
-	    },
-	    {
-	      title: "Last Word",
-				instructions: "Step One: shots",
-				description: "More delicious",
-				season: "Spring",
-				image_url: "http://pixelatedcrumb.com/wp-content/uploads/2012/05/mint_julep-1.jpg"
+				image_url: "https://s-media-cache-ak0.pinimg.com/236x/e9/a6/c2/e9a6c2714daefb70ea6922f4282d90ca.jpg"
 	    }
 	  ]
 
